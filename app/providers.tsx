e958f9cbb45d95db382f74c36fb97191b92302a3
h5371h <htsd18@gmail.com>
2024-10-15 01:45:38 -0700

    changed providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import { ThemeProvider } from 'next-themes';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
+import { useEffect } from 'react';
 import '@aws-amplify/ui-react/styles.css';
-import { amplifyConfig } from './amplify-config';
+import amplifyconfig from '@/amplify_outputs.json';
 
-// Ensure all required environment variables are set
 const config = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOLS_ID ?? '',
-      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOLS_WEB_CLIENT_ID ?? '',
-      region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION ?? '',
-    },
+      userPoolId: amplifyconfig.auth.user_pool_id,
+      userPoolClientId: amplifyconfig.auth.user_pool_client_id,
+      region: amplifyconfig.auth.aws_region,
+    }
   },
   API: {
     GraphQL: {
-      endpoint: process.env.NEXT_PUBLIC_APPSYNC_GRAPHQL_ENDPOINT ?? '',
-      region: process.env.NEXT_PUBLIC_AWS_APPSYNC_REGION ?? '',
-      defaultAuthMode: 'apiKey',
-      apiKey: process.env.NEXT_PUBLIC_AWS_APPSYNC_API_KEY ?? '',
-    },
-  },
+      endpoint: amplifyconfig.data.url,
+      region: amplifyconfig.data.aws_region,
+      defaultAuthMode: amplifyconfig.data.default_authorization_type,
+      apiKey: amplifyconfig.data.api_key
+    }
+  }
 };
 
-Amplify.configure(config);
-
 export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    Amplify.configure(config);
+  }, []);
+
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <Authenticator.Provider>{children}</Authenticator.Provider>
-    </ThemeProvider>
+    <Authenticator.Provider>
+      {children}
+    </Authenticator.Provider>
   );
 }
